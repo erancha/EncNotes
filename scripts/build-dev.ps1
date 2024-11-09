214b3f95e531affee68267e0bccaac2f911372bb
Eran Hachmon <erancha@gmail.com>
2024-11-09 13:16:54 +0200

    (1) Continuation of a previous commit: 5505c2d Relocated template.yaml into scripts\
    (2) Updating the appConfig.json file only if the stack was actually updated in AWS.

--- scripts/build-dev.ps1
+++ scripts/build-dev.ps1
@@ -110,23 +110,23 @@ if ($LASTEXITCODE -eq 0) {
             }
             else {
                 Write-Output "`nDeployment completed successfully."
-            }
 
-            # Update the appConfig.json file
-            $stack_outputs = .\get-stack-outputs.ps1
-            $rest_api_url = .\get-api-url.ps1 -stack_outputs $stack_outputs -TYPE 'Rest'
-            $websocket_api_url = .\get-api-url.ps1 -stack_outputs $stack_outputs -TYPE 'WebSocket'
-            $redirect_sign_in = "http://localhost:3000" # /callback
-            $redirect_sign_out = "http://localhost:3000" # /logout
-           
-            .\generate-config-content.ps1 `
-                -stack_outputs $stack_outputs `
-                -rest_api_url $rest_api_url `
-                -websocket_api_url $websocket_api_url `
-                -backend_build_time `
-                -redirect_sign_in $redirect_sign_in `
-                -redirect_sign_out $redirect_sign_out
-            Copy-Item -Path $CONFIG_FILE_PATH -Destination $LAST_DEV_CONFIG_FILE_PATH
+                # Update the appConfig.json file
+                $stack_outputs = .\get-stack-outputs.ps1
+                $rest_api_url = .\get-api-url.ps1 -stack_outputs $stack_outputs -TYPE 'Rest'
+                $websocket_api_url = .\get-api-url.ps1 -stack_outputs $stack_outputs -TYPE 'WebSocket'
+                $redirect_sign_in = "http://localhost:3000" # /callback
+                $redirect_sign_out = "http://localhost:3000" # /logout
+            
+                .\generate-config-content.ps1 `
+                    -stack_outputs $stack_outputs `
+                    -rest_api_url $rest_api_url `
+                    -websocket_api_url $websocket_api_url `
+                    -backend_build_time `
+                    -redirect_sign_in $redirect_sign_in `
+                    -redirect_sign_out $redirect_sign_out
+                Copy-Item -Path $CONFIG_FILE_PATH -Destination $LAST_DEV_CONFIG_FILE_PATH
+            }
         }
     }
     else {
